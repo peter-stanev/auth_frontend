18a19f02c6a4c8194bee3a9a4fe0ca999495258f
Peter Stanev <peter.stanev.bul@gmail.com>
2016-02-24 19:56:33 +0200

    update app

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -17,30 +17,7 @@
 
   angular
     .module('komae', [
-      'app.core',
-      'app.routes',
-      'app.sidebar',
-      'app.navsearch',
-      'app.preloader',
-      'app.loadingbar',
-      'app.translate',
-      'app.settings',
-      'app.dashboard',
-      'app.icons',
-      'app.flatdoc',
-      'app.notify',
-      'app.bootstrapui',
-      'app.elements',
-      'app.panels',
-      'app.charts',
-      'app.forms',
-      'app.locale',
-      'app.maps',
-      'app.pages',
-      'app.tables',
-      'app.extras',
-      'app.mailbox',
-      'app.utils'
+      'app.core'
     ])
     .constant('config', {
       apiBaseUrl: 'http://localhost:3000/'
